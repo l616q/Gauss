ba6351a9ade0d84f1d970f60a3fae4eaecba0561
kingqluo <511470083@qq.com>
2021-05-27 10:44:12 +0800

    update

--- entity/feature_config.py
+++ entity/feature_config.py
@@ -3,37 +3,36 @@
 # Copyright (c) 2020, Citic Inc. All rights reserved.
 # Authors: Lab
 
-
 from collections import defaultdict
+from entity import Entity
 
 class FeatureItemConf(object):
 
-    def __init__(self, name, dtype, start_postion, range, 
+    def __init__(self, name, index, size=1, dtype="float32", 
                  default_value=None, ftype="numerical"):
         assert dtype in ("int64", "float32", "string", "bool", "date")
-        self._name = name
-        self._dtype = dtype
-        self._start_postion = start_postion
-        self._range = range
+        assert ftype in ("numerical", "catagory")
+        self.name = name
+        self.dtype = dtype
+        self.index = index
+        self.size = size
         if default_value is None:
-            if _dtype == "string" or _dtype == "date":
-                self._default_value = "UNK"
+            if dtype == "string" or dtype == "date":
+                self.default_value = "UNK"
             else:
-                self._default_value = 0
+                self.default_value = 0
         else:
-            self._default_value = default_value
-        self._ftype = ftype
-    def reset_feature_type(ftype):
-        self._ftype = _ftype
-class FeatureConf(object):
+            self.default_value = default_value
+        self.ftype = ftype
+class FeatureConf(Entity):
     def __init__(self, name, file_path):
-        self._name = name
         self._file_path = file_path
         self._dict = {}
+        super(Entity, self).__init__(
+            name = name,
+        )
     def parse():
     def reset_feature_type(key, ftype):
-    def set_feature_file(file_path):
-        self._file_path = file_path
 
 
         
